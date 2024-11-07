5cab7fde4d7ab5d731415bf44f9c841481b7cf06
Matias Lazaro <mglp2404@gmail.com>
2024-11-06 22:08:30 -0500

    add module/predictions/ :
    Se crea el modulo de predictions

---
+++ src/app/modules/guilletotal/predictions/pages/predictions/predictions.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SubscriptionComponent } from '../../../../Payment/subscription/subscription.component';
+import { MatDialog } from '@angular/material/dialog';
+@Component({
+  selector: 'app-home',
+  templateUrl: './predictions.component.html',
+  styleUrls: ['./predictions.component.scss']
+})
+export class HomeComponent {
+  constructor(private dialog: MatDialog) {}
+
+  // Método para abrir el modal de suscripción
+  openSubscriptionModal(): void {
+    this.dialog.open(SubscriptionComponent, {
+      width: '450px',
+      height: '550px',
+      panelClass: 'custom-modal'
+    });
+  }
+}
